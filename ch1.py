e5bcc1fc05148a4c4dc469d1a9ebab7617878a1e
HallPC <hall@pc.com>
2019-12-16 20:19:31 +0100

    Initial commit

---
+++ ch1.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+
+def search(lines, pattern, history=5):
+	previous_lines = deque(maxlen=history)
+	for line in lines:
+		if pattern in line:
+			yield line, previous_lines
+		previous_lines.append(line)
+
+
+def main_search():
+	with open('file.txt') as f:
+		for line, prevlines in search(f, 'python', 5):
+			print(f'line: {line.strip()}')
+			print(f'prev: {prevlines}')
+
+
+main_search()
+
+
+de = deque(maxlen=3)
+de.extend([1, 2, 3])
+print(de)
+de.append(4)
+print(de)
+de.appendleft(1)
+print(de)
